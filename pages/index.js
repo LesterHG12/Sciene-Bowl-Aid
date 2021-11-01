a14a346e66aa4e8daeb8948a7e3b60c006385829
LesterHG12 <92682812+LesterHG12@users.noreply.github.com>
2021-11-01 07:47:54 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+/** @format */
+import { FaArrowRight } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { GiMaterialsScience } from "react-icons/gi";
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='heading-container'>
+        <GiMaterialsScience size={50} />
+        <h1 className='heading'>Science Bowl Aid</h1>
+      </div>
+      <div className='hero-container'>
+        <h1 className='hero-title'>Practice for National Science Bowl</h1>
+        <p className='hero-body'>
+          Science Bowl Aid is a simple, open-source, and intuitive platform to
+          time and score practices while accessing official Science Bowl
+          questions with just a few clicks.
+        </p>
+        <div className='hero-buttons'>
+          <a href='/scienceBowl'>
+            <button className='action-button'>
+              Science Bowl
+              <FaArrowRight size={20} className='button-icon' />
+            </button>
+          </a>
+          <a href='https://github.com/Sarthak-Dayal/Science-Bowl-Aid'>
+            <button className='github-button'>
+              <AiFillGithub className='github-icon' />
+              GitHub
+            </button>
+          </a>
+          <a href='/quizBowl'>
+            <button className='action-button'>
+              Quiz Bowl 
+              <FaArrowRight size={20} className='button-icon' />
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/scienceBowl.js
@@ -0,0 +1,81 @@
+/** @format */
+import { useEffect, useState } from "react";
+import Navbar from "../Components/ScienceBowl/navbar";
+import RoundComponent from "../Components/ScienceBowl/component";
+import Timer from "../Components/timer";
+import Scorer from "../Components/scorer";
+import Draggable from "react-draggable";
+import Head from "next/head";
+
+export default function App() {
+  const [setNum, setSetNum] = useState(1);
+  const [roundNum, setRoundNum] = useState(1);
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const set = urlParams.get("set");
+    const round = urlParams.get("round");
+
+    if (set >= 1 && set <= 12 && round >= 1 && round <= 17) {
+      if (!(round > 15 && (set === 2 || set === 5 || set === 6))) {
+        setSetNum(set);
+        setRoundNum(round);
+      } else {
+        setSetNum(1);
+        setRoundNum(1);
+        window.location = `/scienceBowl?set=1&round=1`;
+      }
+    } else {
+      setSetNum(1);
+      setRoundNum(1);
+      window.location = `/scienceBowl?set=1&round=1`;
+    }
+  });
+
+  return (
+    <div>
+      <Head></Head>
+      <div className='container'>
+        <Navbar />
+        <RoundComponent set={setNum} round={roundNum} />
+        <Draggable>
+          <div
+            style={{ position: "fixed", left: "86vw", top: "70px" }}
+            className='draggable'>
+            <Timer className='timer' />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div
+            style={{ position: "fixed", left: 20, top: "86vh" }}
+            className='draggable'>
+            <Scorer team={1} />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div
+            style={{ position: "fixed", left: "80vw", top: "86vh" }}
+            className='draggable'>
+            <Scorer team={2} />
+          </div>
+        </Draggable>
+
+        <div
+          style={{ position: "fixed", left: "1vw", bottom: "3vh" }}
+          className='mobile-only'>
+          <Timer className='timer' />
+        </div>
+        <div
+          style={{ position: "fixed", right: "1vw", bottom: "11.8vh" }}
+          className='mobile-only'>
+          <Scorer team={1} />
+        </div>
+        <div
+          style={{ position: "fixed", right: "1vw", bottom: "2vh" }}
+          className='mobile-only'>
+          <Scorer team={2} className='Scorer' />
+        </div>
+      </div>
+    </div>
+  );
+}
